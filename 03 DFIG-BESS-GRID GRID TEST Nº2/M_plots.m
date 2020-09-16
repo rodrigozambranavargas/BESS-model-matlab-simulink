4434d41a55caff3e972772f7bbb8d665cc2929c2
rodrigo.zambrana <rodrigo.zambrana.vargas@gmail.com>
2020-09-16 15:22:32 -0300

    Islanding tests

--- 03 DFIG-BESS-GRID GRID TEST Nº2/M_plots.m
+++ 03 DFIG-BESS-GRID GRID TEST Nº2/M_plots.m
@@ -1,109 +1,24 @@
-%% Plots of DFIG %%
-
-%plot(wind_plot(:,1),wind_plot(:,2));
-%plot(Rotor_speed_plot(:,1),Rotor_speed_plot(:,2));
-%plot(torque_MPPT_plot(:,1),torque_MPPT_plot(:,2),'b',torque_MPPT_plot(:,1),torque_MPPT_plot(:,3),'g');
-%plot(Current_id_rotor_plot(:,1),Current_id_rotor_plot(:,2),'b',Current_id_rotor_plot(:,1),Current_id_rotor_plot(:,3),'g');
-%plot(Current_iq_rotor_plot(:,1),Current_iq_rotor_plot(:,2),'b',Current_iq_rotor_plot(:,1),Current_iq_rotor_plot(:,3),'g');
-%plot(Current_rotor_plot(:,1),Current_rotor_plot(:,2),'b',Current_rotor_plot(:,1),Current_rotor_plot(:,3),'g',Current_rotor_plot(:,1),Current_rotor_plot(:,4),'r');
-
-%plot(Vbus_plot(:,1),Vbus_plot(:,2),'b',Vbus_plot(:,1),Vbus_plot(:,3),'g');
-%plot(Current_id_grid_plot(:,1),Current_id_grid_plot(:,2),'b',Current_id_grid_plot(:,1),Current_id_grid_plot(:,3),'g');
-%plot(Current_iq_grid_plot(:,1),Current_iq_grid_plot(:,2),'b',Current_iq_grid_plot(:,1),Current_iq_grid_plot(:,3),'g');
-%plot(Stator_rotor_total_power_plot(:,1),Stator_rotor_total_power_plot(:,2),'b',Stator_rotor_total_power_plot(:,1),Stator_rotor_total_power_plot(:,3),'g');
-
-% %figura 1
-% subplot(3,1,1);
-% plot(wind_plot(:,1),wind_plot(:,2));
-% title('Mundaça de Sinal de Vento')
-% subplot(3,1,2); 
-% plot(Rotor_speed_plot(:,1),Rotor_speed_plot(:,2));
-% title('Velocidade do Rotor')
-% subplot(3,1,3); 
-% plot(torque_MPPT_plot(:,1),torque_MPPT_plot(:,3),'r',torque_MPPT_plot(:,1),torque_MPPT_plot(:,2),'b');
-% title('Torque no Exio do DFIG')
-
-% %fugure 2
-% subplot(2,2,1);
-% plot(Current_id_rotor_plot(:,1),Current_id_rotor_plot(:,3),'r',Current_id_rotor_plot(:,1),Current_id_rotor_plot(:,2),'b');
-% title('Corrente Id')
-% subplot(2,2,2); 
-% plot(Current_iq_rotor_plot(:,1),Current_iq_rotor_plot(:,3),'r',Current_iq_rotor_plot(:,1),Current_iq_rotor_plot(:,2),'b');
-% title('Corrente Iq')
-% subplot(2,2,[3,4]); 
-% plot(Current_rotor_plot(:,1),Current_rotor_plot(:,2),'b',Current_rotor_plot(:,1),Current_rotor_plot(:,3),'g',Current_rotor_plot(:,1),Current_rotor_plot(:,4),'r');
-% title('Corrente do Rotor')
-
-% %fugure 3
-% subplot(2,2,1);
-% plot(Current_id_grid_plot(:,1),Current_id_grid_plot(:,3),'r',Current_id_grid_plot(:,1),Current_id_grid_plot(:,2),'b');
-% title('Corrente Id')
-% subplot(2,2,2); 
-% plot(Current_iq_grid_plot(:,1),Current_iq_grid_plot(:,3),'r',Current_iq_grid_plot(:,1),Current_iq_grid_plot(:,2),'b');
-% title('Corrente Iq')
-% subplot(2,2,[3,4]); 
-% plot(Vbus_plot(:,1),Vbus_plot(:,3),'r',Vbus_plot(:,1),Vbus_plot(:,2),'b');
-% title('Tensão do Barramento CC')
-
-% %fugure 4
-% subplot(2,2,1);
-% plot(Stator_rotor_total_power_plot(:,1),Stator_rotor_total_power_plot(:,2),'b');
-% title('Potência do Estator')
-% subplot(2,2,2); 
-% plot(Stator_rotor_total_power_plot(:,1),Stator_rotor_total_power_plot(:,3),'b');
-% title('Potência do Rotor')
-% subplot(2,2,[3,4]); 
-% plot(Total_power_plot(:,1),Total_power_plot(:,2),'b');
-% title('Potência Total')
-
-%% Plots of BESS %%
-
-% %fugure 1
-% subplot(2,2,1);
-% plot(F_plot(:,1),F_plot(:,2),'b');
-% title('Frequência')
-% subplot(2,2,2); 
-% plot(DC_charge_plot(:,1),DC_charge_plot(:,2),'r',DC_charge_plot(:,1),DC_charge_plot(:,3),'b');
-% title('Tensão do Barramento CC')
-% subplot(2,2,[3,4]); 
-% plot(SOC_plot(:,1),SOC_plot(:,2),'b');
-% title('Estado de Carga SOC')
-
-%fugure 2
-% subplot(2,2,1);
-% plot(Id_plot(:,1),Id_plot(:,2),'b',Id_plot(:,1),Id_plot(:,3),'r');
-% title('Corrente Id')
-% subplot(2,2,2); 
-% plot(Iq_plot(:,1),Iq_plot(:,3),'r',Iq_plot(:,1),Iq_plot(:,2),'b');
-% title('Corrente Iq')
-% subplot(2,2,[3,4]); 
-% plot(Power_plot(:,1),Power_plot(:,3),'b',Power_plot(:,1),Power_plot(:,2),'r',Power_plot(:,1),Power_plot(:,4),'g');
-% title('Potência')
-
-%% Plots of GRID %%
-
-%figure (1)
-%subplot(2,2,1);
-%plot(T1_P_DFIG_CBESS(:,1),T1_P_DFIG_CBESS(:,2),'r');
-%title('Potência DFIG')
-%subplot(2,2,2);
-%plot(T1_P_CBESS(:,1),T1_P_CBESS(:,2),'r');
-%title('Potência BESS')
-%subplot(2,2,[3,4]); 
-%plot(T1_TP_DFIG_CBESS(:,1),T1_TP_DFIG_CBESS(:,2),'r',T1_TP_DFIG_SBESS(:,1),T1_TP_DFIG_SBESS(:,2),'b');
-%title('Potência Sistema DFIG/BESS')
-
-%figure (2)
-%plot(T1_F_CBESS(:,1),T1_F_CBESS(:,2),'r',T1_F_SBESS(:,1),T1_F_SBESS(:,2),'b');
-%title('Potência Sistema DFIG/BESS')
-
-%%
+%Frequency DFIG
 figure (1)
-plot(f_dfig_test1(:,1),f_dfig_test1(:,2),'r',f_dfig_test2(:,1),f_dfig_test2(:,2),'b',f_dfig_test3(:,1),f_dfig_test3(:,2),'g',f_dfig_test4(:,1),f_dfig_test4(:,2),'m');
-%plot(f_dfig_test1(:,1),f_dfig_test1(:,2),'r',f_dfig_test2(:,1),f_dfig_test2(:,2),'b',f_dfig_test3(:,1),f_dfig_test3(:,2),'g',f_dfig_test4(:,1),f_dfig_test4(:,2),'m');
+plot(f_dfig_test5(:,1),f_dfig_test5(:,2),'r',f_dfig_test6(:,1),f_dfig_test6(:,2),'b',f_dfig_test7(:,1),f_dfig_test7(:,2),'g',f_dfig_test8(:,1),f_dfig_test8(:,2),'r',f_dfig_test9(:,1),f_dfig_test9(:,2),'b',f_dfig_test10(:,1),f_dfig_test10(:,2),'g',f_dfig_test11(:,1),f_dfig_test11(:,2),'r',f_dfig_test12(:,1),f_dfig_test12(:,2),'b',f_dfig_test13(:,1),f_dfig_test13(:,2),'g');
+% figure (5)
+% plot(f_dfig_test5(:,1),f_dfig_test5(:,2),'r',f_dfig_test6(:,1),f_dfig_test6(:,2),'b',f_dfig_test7(:,1),f_dfig_test7(:,2),'g');
+% figure (6)
+% plot(f_dfig_test8(:,1),f_dfig_test8(:,2),'r',f_dfig_test9(:,1),f_dfig_test9(:,2),'b',f_dfig_test10(:,1),f_dfig_test10(:,2),'g');
+% figure (7)
+% plot(f_dfig_test11(:,1),f_dfig_test11(:,2),'r',f_dfig_test12(:,1),f_dfig_test12(:,2),'b',f_dfig_test13(:,1),f_dfig_test13(:,2),'g');
+figure (8)
+plot(f_dfig_test14(:,1),f_dfig_test14(:,2),'r',f_dfig_test15(:,1),f_dfig_test15(:,2),'b',f_dfig_test16(:,1),f_dfig_test16(:,2),'g');
 
+%Frequency GRID
 %figure (2)
-%plot(f_grid_test1(:,1),f_grid_test1(:,2),'r',f_grid_test2(:,1),f_grid_test2(:,2),'b',f_grid_test3(:,1),f_grid_test3(:,2),'g',f_grid_test4(:,1),f_grid_test4(:,2),'m');
+%plot(f_grid_test5(:,1),f_grid_test5(:,2),'r',f_grid_test6(:,1),f_grid_test6(:,2),'b',f_grid_test7(:,1),f_grid_test7(:,2),'g',f_grid_test8(:,1),f_grid_test8(:,2),'r',f_grid_test9(:,1),f_grid_test9(:,2),'b',f_grid_test10(:,1),f_grid_test10(:,2),'g',f_grid_test11(:,1),f_grid_test11(:,2),'r',f_grid_test12(:,1),f_grid_test12(:,2),'b',f_grid_test13(:,1),f_grid_test13(:,2),'g');
 
+%% Power ref Tests 
+% figure (3)
+% plot(P_droop_ref_test6(:,1),P_droop_ref_test6(:,2),'r',Inertial_response_ref_test6(:,1),Inertial_response_ref_test6(:,2),'b',P_system_ref_test6(:,1),P_system_ref_test6(:,2),'r',P_bess_ref_test7(:,1),P_bess_ref_test7(:,2),'g',P_droop_ref_test9(:,1),P_droop_ref_test9(:,2),'r',Inertial_response_ref_test9(:,1),Inertial_response_ref_test9(:,2),'b',P_system_ref_test9(:,1),P_system_ref_test9(:,2),'r',P_bess_ref_test10(:,1),P_bess_ref_test10(:,2),'g',P_droop_ref_test12(:,1),P_droop_ref_test12(:,2),'r',Inertial_response_ref_test12(:,1),Inertial_response_ref_test12(:,2),'b',P_system_ref_test12(:,1),P_system_ref_test12(:,2),'r')
+% Power mean Tests 
+% figure (4)
+% plot(P_dfig_mean_test5(:,1),P_dfig_mean_test5(:,2),'r',P_dfig_mean_test6(:,1),P_dfig_mean_test6(:,2),'b',P_bess_mean_test7(:,1),P_bess_mean_test7(:,2),'g',P_dfig_mean_test8(:,1),P_dfig_mean_test8(:,2),'r',P_dfig_mean_test9(:,1),P_dfig_mean_test9(:,2),'b',P_bess_mean_test10(:,1),P_bess_mean_test10(:,2),'g',P_dfig_mean_test11(:,1),P_dfig_mean_test11(:,2),'r',P_dfig_mean_test12(:,1),P_dfig_mean_test12(:,2),'b');
 
 
